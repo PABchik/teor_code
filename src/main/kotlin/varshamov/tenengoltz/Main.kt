300077a7543aec70e957bbe4b09c9c3d803930a8
pavel_batotsyrenov <webmoney91rus@yandex.ru>
2020-11-09 01:30:30 +0300

    complete alg + tests, fixed bugs

--- src/main/kotlin/varshamov/tenengoltz/Main.kt
+++ src/main/kotlin/varshamov/tenengoltz/Main.kt
@@ -1,13 +1,75 @@
 package varshamov.tenengoltz
 
+import sun.util.calendar.CalendarUtils.mod
 import kotlin.math.pow
 import kotlin.math.roundToInt
 
 fun main() {
     val n = readLine()!!.toInt()
     val validNlenCodeWords = getAvailableCodes(n)
-    print("All valid code words with length $n : $validNlenCodeWords");
+    println("All valid code words with length $n : $validNlenCodeWords")
+    val receivedWord = readLine()!!
+    println("Corrected word is ${correctWord(receivedWord, n)}")
+}
 
+fun correctWord(receivedWord: String, n: Int): String {
+    if (receivedWord.isEmpty() || receivedWord.length !in arrayOf(n - 1, n + 1)) {
+        println("Cannot correct received word")
+        return receivedWord
+    }
+    when (receivedWord.length) {
+        n + 1 -> return delFun(receivedWord)
+        n - 1 -> return addFun(receivedWord)
+    }
+    return receivedWord
+}
+
+fun addFun(word: String): String {
+    val T = mod(-countS(word), word.length + 2)
+    val weight = word.replace("0", "").length
+    return when (T) {
+        in 0..weight -> addSym(word, T, '1')
+        in weight + 1..word.length + 1 -> addSym(word, word.length + 1 - T, '0')
+        else -> word
+    }
+}
+
+fun addSym(word: String, T: Int, sym: Char): String {
+    val index = getIndex(word, T, sym)
+    return (word.reversed().slice(0 until index) + (if (sym == '0') "1" else "0") + word.reversed().slice(index..word.lastIndex)).reversed()
+}
+
+fun delFun(word: String): String {
+    val T = mod(countS(word), word.length)
+    val weight = word.replace("0", "").length
+    return when (T) {
+        0 -> word.slice(0 until word.lastIndex)
+        weight -> word.slice(1..word.lastIndex)
+        in 0 until weight -> delSym(word, T, '1')
+        in weight + 1..word.length -> delSym(word, word.length - T, '0')
+        else -> word
+    }
+}
+
+fun delSym(word: String, T: Int, sym: Char): String {
+    val rmIndex = getIndex(word, T, sym)
+    return word.reversed().removeRange(rmIndex..rmIndex).reversed()
+}
+
+fun getIndex(word: String, T: Int, sym: Char): Int {
+    var t = T
+    var index = 0
+    if (t != 0) {
+        for ((i, ch) in word.reversed().withIndex()) {
+            if (ch == sym && t != 0) {
+                if (--t == 0) {
+                    index = i + 1
+                    break
+                }
+            }
+        }
+    }
+    return index
 }
 
 fun getAvailableCodes(n: Int): List<String> {
--- src/test/kotlin/varshamov/tenengoltz/MainKtTest.kt
+++ src/test/kotlin/varshamov/tenengoltz/MainKtTest.kt
@@ -21,4 +21,79 @@ class MainKtTest {
         assertEquals(7, countS("0011"))
     }
 
+    @Test
+    fun checkInsertCorrection() {
+        assertEquals("000000", correctWord("0000000", 6))
+        assertEquals("000000", correctWord("0000001", 6))
+        assertEquals("000000", correctWord("1000000", 6))
+        assertEquals("000000", correctWord("0100000", 6))
+        assertEquals("000000", correctWord("0010000", 6))
+        assertEquals("000000", correctWord("0001000", 6))
+        assertEquals("000000", correctWord("0000100", 6))
+        assertEquals("000000", correctWord("0000010", 6))
+
+        assertEquals("100001", correctWord("1000101", 6))
+        assertEquals("100001", correctWord("1010001", 6))
+        assertEquals("100001", correctWord("1001001", 6))
+        assertEquals("100001", correctWord("1000011", 6))
+        assertEquals("100001", correctWord("1000001", 6))
+        assertEquals("100001", correctWord("1000001", 6))
+        assertEquals("100001", correctWord("1000001", 6))
+        assertEquals("100001", correctWord("1100001", 6))
+
+        assertEquals("111111", correctWord("1111111", 6))
+        assertEquals("111111", correctWord("1101111", 6))
+        assertEquals("111111", correctWord("1110111", 6))
+        assertEquals("111111", correctWord("1111011", 6))
+        assertEquals("111111", correctWord("0111111", 6))
+        assertEquals("111111", correctWord("1111110", 6))
+        assertEquals("111111", correctWord("1011111", 6))
+        assertEquals("111111", correctWord("1111101", 6))
+
+        assertEquals("00111", correctWord("000111", 5))
+        assertEquals("00111", correctWord("001111", 5))
+        assertEquals("00111", correctWord("010111", 5))
+        assertEquals("00111", correctWord("000111", 5))
+        assertEquals("00111", correctWord("100111", 5))
+        assertEquals("00111", correctWord("001101", 5))
+        assertEquals("00111", correctWord("001011", 5))
+        assertEquals("00111", correctWord("001110", 5))
+
+        assertEquals("1001", correctWord("11001", 4))
+        assertEquals("1001", correctWord("01001", 4))
+        assertEquals("1001", correctWord("10011", 4))
+        assertEquals("1001", correctWord("10010", 4))
+        assertEquals("1001", correctWord("10001", 4))
+        assertEquals("1001", correctWord("10101", 4))
+        assertEquals("1001", correctWord("11001", 4))
+        assertEquals("1001", correctWord("10011", 4))
+    }
+
+    @Test
+    fun checkDeleteCorrection() {
+        assertEquals("000000", correctWord("00000", 6))
+
+        assertEquals("001011", correctWord("00101", 6))
+        assertEquals("001011", correctWord("00101", 6))
+        assertEquals("001011", correctWord("00111", 6))
+        assertEquals("001011", correctWord("00011", 6))
+        assertEquals("001011", correctWord("01011", 6))
+        assertEquals("001011", correctWord("01011", 6))
+
+        assertEquals("111111", correctWord("11111", 6))
+
+        assertEquals("00111", correctWord("0011", 5))
+        assertEquals("00111", correctWord("0111", 5))
+
+        assertEquals("1001", correctWord("001", 4))
+        assertEquals("1001", correctWord("100", 4))
+        assertEquals("1001", correctWord("101", 4))
+
+    }
+
+    @Test
+    fun checkFailedDelete() {
+        assertEquals("1001", correctWord("001", 4))
+        assertEquals("001011", correctWord("00011", 6))//failed
+    }
 }
